a597658a9f427fcca7aeac4ebe37865e50fe3312
nieny225 <nieny225@gmail.com>
2023-11-16 17:34:18 +0800

    Support Web Screenshot block

--- src/client/main.ts
+++
@@ -1,5 +0,0 @@
-//import './css/bootstrap-icons_font_bootstrap-icons.css'
-import {OmniSDKClient} from 'omni-sdk';
-const sdk = new OmniSDKClient("<replace with your extension id>").init();
-
-sdk.sendMessageToSession("Hello from Omni Extension!");
--- src/server/extension.ts
+++ src/server/extension.ts
@@ -1,5 +1,60 @@
+
+// Import required modules
+import { OAIBaseComponent, WorkerContext, OmniComponentMacroTypes, BlockCategory as Category } from 'omni-sockets';
+import puppeteer from 'puppeteer';
+
+// Create the WebScreenshotComponent
+const webScreenshotComponent = OAIBaseComponent.create('webscreenshot', 'web-screenshot')
+  .fromScratch()
+  .set('title', 'Web Screenshot')
+  .set('category', 'Data Extraction')
+  .set('description', 'Capture a screenshot of a web page using Puppeteer.')
+  .setMethod('X-CUSTOM')
+  .setMeta({
+    source: {
+      summary: 'Capture a screenshot of a web page using Puppeteer.',
+      links: {
+        'Puppeteer Github': 'https://github.com/puppeteer/puppeteer',
+      },
+    },
+  })
+webScreenshotComponent
+  .addInput(
+    webScreenshotComponent.createInput('url', 'string')
+      .set('title', 'URL')
+      .set('description', 'The URL of the web page to capture.')
+      .setRequired(true)
+      .toOmniIO()
+  )
+  .addOutput(
+    webScreenshotComponent.createOutput('screenshot', 'string', 'image')
+      .set('title', 'Screenshot')
+      .set('description', 'The captured screenshot as a base64 encoded image.')
+      .toOmniIO()
+  )
+  .setMacro(OmniComponentMacroTypes.EXEC, async (payload: any, ctx: WorkerContext) => {
+    const browser = await puppeteer.launch({
+      defaultViewport: {
+        width: 1280,
+        height: 2000,
+      },
+    });
+
+    const page = await browser.newPage();
+    await page.goto(payload.url);
+
+    const screenshotBuffer = await page.screenshot();
+    const screenshotBase64 = screenshotBuffer.toString('base64');
+
+    await browser.close();
+
+    return { screenshot: screenshotBase64 };
+  });
+
+// Export the extension
 export default {
-  init: (ctx: any) => {
-    ctx.app.info('Omni Extension Template loaded')
-  }
-}
+  createComponents: () => ({
+    blocks: [webScreenshotComponent.toJSON()],
+    patches: [],
+  }),
+};
