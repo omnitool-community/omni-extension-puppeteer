e08d7ee7351dd8b1616c504fdc5e4654aff2c588
nieny225 <nieny225@gmail.com>
2023-11-17 14:21:50 +0800

    Workaround dynamic require issue

--- src/build.mjs
+++ src/build.mjs
@@ -1,6 +1,9 @@
 import esbuild from 'esbuild';
 
-console.log("Building omni-extension-puppeteer....")
+console.log("Building omni-extension-puppeteer....");
+
+const ESM_REQUIRE_SHIM = `await(async()=>{let{dirname:e}=await import("path"),{fileURLToPath:i}=await import("url");if(typeof globalThis.__filename>"u"&&(globalThis.__filename=i(import.meta.url)),typeof globalThis.__dirname>"u"&&(globalThis.__dirname=e(globalThis.__filename)),typeof globalThis.require>"u"){let{default:a}=await import("module");globalThis.require=a.createRequire(import.meta.url)}})();`;
+
 await esbuild.build({
     entryPoints: ['./extension.ts'],
     outdir: '../server/',
@@ -9,5 +12,11 @@ await esbuild.build({
     target: ['esNext'],
     format: 'esm',
     external: ['omni-sockets'],
-    color: true
-}).catch(() => process.exit(1))
+    color: true,
+    banner: {
+        js: ESM_REQUIRE_SHIM
+    }
+}).catch((error) => {
+    console.error('Build failed with error:', error);
+    process.exit(1);
+});
